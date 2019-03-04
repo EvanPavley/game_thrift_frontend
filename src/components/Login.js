191ba2dd35e07a133818ae4f58e0010201614aed
EvanPavley <github email address>
2019-03-04 15:35:29 -0500

    Created simple controlled forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import '../css/Login.css';
 
 class Login extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+  }
+
+  handleChange = e => {
+  this.setState({
+    [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state);
+    // this.sendFormDataSomewhere(this.state)
+  }
+
   render() {
     return (
-      <h1>LOGIN</h1>
+      <div className='login-container'>
+        <h1>LOGIN</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Username:</label>
+          <input
+            name='username'
+            value={this.state.username}
+            onChange={this.handleChange}
+            className='login-input'
+            /><br/>
+          <label>Email:</label>
+          <input
+            name='email'
+            value={this.state.email}
+            onChange={this.handleChange}
+            className='login-input'
+            /><br/>
+          <label>Password:</label>
+          <input
+            name='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+            className='login-input'
+            /><br/>
+          <input
+            type="submit"
+            value="submit"
+            className='login-submit'
+            />
+        </form>
+      </div>
     );
   }
 }
--- src/components/NewGameForm.js
+++ src/components/NewGameForm.js
@@ -1,10 +1,97 @@
 import React, { Component } from 'react';
-import './GameCard'
+import '../css/NewGameForm.css';
 
 class NewGameForm extends Component {
+  state = {
+    isGame: true,
+    name: '',
+    price: '',
+    image: '',
+    console: '',
+    description: '',
+    releasedDate: '',
+  }
+
+  handleChange = e => {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+      })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state);
+    // this.sendFormDataSomewhere(this.state)
+  }
+
   render() {
     return (
-      <h1>NEW GAME FORM</h1>
+      <div className='ngf-container'>
+        <h1>NEW GAME FORM</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Is Game?:</label>
+          <input
+            name= 'isGame'
+            value={this.state.isGame}
+            onChange={this.handleChange}
+            type="checkbox"
+            checked={this.state.isGame}
+            className='ngf-input'
+            /><br/>
+          <label>Name:</label>
+          <input
+            name= 'name'
+            value={this.state.name}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <label>Price:</label>
+          <input
+            name= 'price'
+            value={this.state.price}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <label>Image:</label>
+          <input
+            name= 'image'
+            value={this.state.image}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <label>Console:</label>
+          <input
+            name= 'console'
+            value={this.state.console}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <label>Description:</label>
+          <input
+            type='textarea'
+            name= 'description'
+            value={this.state.description}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <label>released date:</label>
+          <input
+            type="date"
+            name= 'releasedDate'
+            value={this.state.releasedDate}
+            onChange={this.handleChange}
+            className='ngf-input'
+            /><br/>
+          <input
+            type="submit"
+            value="submit"
+            className='ngf-submit'
+            />
+        </form>
+      </div>
     );
   }
 }
